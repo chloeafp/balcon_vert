05250f1909637b4527a9b666b31c69001433ed9d
chloeafp <chloeafp@gmail.com>
2023-06-28 15:57:41 +0200

    initialisation projet

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+
+const router = require("./router");
+
+app.use(router);
+
+module.exports = app;
